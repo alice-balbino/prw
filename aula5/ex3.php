83daaf165f95c2e54c24090f815c83e78f1c547b
alice <aliceusaoculos@gmail.com>
2022-03-23 11:04:30 -0300

    exemplo switch case

---
+++ aula5/ex3.php
@@ -0,0 +1,9 @@
+<?php
+    $op = "a";
+    switch($op){
+        case "a": echo "Abrir"; break;
+        case "a": echo "Fechar"; break;
+        case "a": echo "Salvar"; break;
+        case "a": echo "Sair"; break;
+    }
+?>
