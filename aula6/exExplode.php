b542dcbf107bdba09ecb6b2f1135fdb358149353
alice <aliceusaoculos@gmail.com>
2022-03-30 10:30:16 -0300

    exemplo explode

---
+++ aula6/exExplode.php
@@ -0,0 +1,9 @@
+<?php
+    $data = date("m-d-Y");
+    $partes = explode("-",$data);
+    $parte1 = $partes[0];
+    $parte2 = $partes[1];
+    $parte3 = $partes[2];
+    $novadata = $parte2 . "/". $parte1 . "/" . $parte3;
+    echo $novadata;
+?>
