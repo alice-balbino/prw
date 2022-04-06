69605b3d334c2bfff9973de4749b125efc96214d
alice <aliceusaoculos@gmail.com>
2022-04-06 09:19:39 -0300

    contador de string

---
+++ lista3/ex1.php
@@ -0,0 +1,10 @@
+<?php
+    /*Crie um script em PHP que exiba quantos caracteres possui
+    uma palavra ou frase que foi atribuída a uma vaiável.*/
+    
+    $string = "alice";
+    $contString = strlen($string);
+    
+    echo "Essa string tem $contString caracteres.";
+    
+?>
