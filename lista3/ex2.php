50a81d65bffa8c9bc023ec5ced7db15067283853
alice <aliceusaoculos@gmail.com>
2022-04-06 09:57:57 -0300

    trocar vogal por x

--- lista3/ex2.php
+++ lista3/ex2.php
@@ -1,5 +1,10 @@
 <?php
     /*Crie um script em PHP que pegue uma palavra contida em
-    uma variável e substitua todas as vogais por X.*/
+    uma variável e substitua todas as vogais por X.
+    */
+
+    $palavra = "sempre";
+    $palnova = str_replace("e","x", $palavra);
+    echo "A palavra era: ".$palavra. "<br>"." Agora é: ".$palnova; 
 
 ?>
