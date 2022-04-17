2d79c6a883f9e6acf195d7a7a91f41845636d218
alice <aliceusaoculos@gmail.com>
2022-04-17 19:04:52 -0300

    tentando

--- lista4/ex1/ex1.php
+++ lista4/ex1/ex1.php
@@ -1,3 +1,32 @@
 <?php
-ndy 
+    $num1;
+    $num2;
+    $value;
+    $i;
+/*
+    if($value ==0){
+        $soma = $num1 + $num2;
+        echo $soma;
+    }
+    if($value ==1){
+        $sub = $num1 - $num2;
+        echo $sub;
+    }
+    if($value ==2){
+        $div = $num1 / $num2;
+        echo $div;
+    }
+    if($value ==3){
+        $mul = $num1 * $num2;
+        echo $mul;
+    }
+*/
+
+for (i = 1; i == 0; i++)
+  {
+    $soma = $num1 + $num2;
+        echo $soma;
+  }
+
+
 ?>
