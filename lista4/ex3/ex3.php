4616a098b35fac2ff7e3c9a220d008617b4ce30c
alice <aliceusaoculos@gmail.com>
2022-04-17 19:23:43 -0300

    estado php

---
+++ lista4/ex3/ex3.php
@@ -0,0 +1,24 @@
+<?php
+
+    $estado;
+    switch($estado){
+        case "rio de janeiro":
+            echo "A capital é: Rio de Janeiro";
+            break;
+        case "são paulo":
+            echo "A capital é: São Paulo";
+            break;
+        case "minas gerais":
+            echo "A capital é: Belo Horizonte";
+            break;
+        case "Espírito Santo":
+            echo "A capital é: Vitória";
+            break;
+        case "paraná":
+            echo "A capital é: Curitiba";
+            break;
+            /*...*/
+    }
+
+
+?>
