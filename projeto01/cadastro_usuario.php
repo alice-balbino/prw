52812c92d1cf51cc4959abb7aaa7838b236109ba
alice <aliceusaoculos@gmail.com>
2022-04-20 10:04:46 -0300

    cadastro certo

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -1,7 +1,7 @@
 <?php
-   $nome_usuario = $_POST['nome'];
-   $email_usuario= $_POST['email'];
-   $fone_usuario = $_POST['telefone'];
+   $nome_usuario = $_POST['nome_usuario'];
+   $email_usuario= $_POST['email_usuario'];
+   $fone_usuario = $_POST['fone_usuario'];
 
     echo "<p>Nome: ".$nome_usuario;
     echo "<p>email: ".$email_usuario;
