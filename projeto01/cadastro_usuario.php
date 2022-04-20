5786a7bc9316d33d1b3fda6200f9329dd92388c6
alice <aliceusaoculos@gmail.com>
2022-04-20 09:59:43 -0300

    cadasrto

---
+++ projeto01/cadastro_usuario.php
@@ -0,0 +1,10 @@
+<?php
+   $nome_usuario = $_POST['nome'];
+   $email_usuario= $_POST['email'];
+   $fone_usuario = $_POST['telefone'];
+
+    echo "<p>Nome: ".$nome_usuario;
+    echo "<p>email: ".$email_usuario;
+    echo "<p>telefone: ".$fone_usuario;
+
+?>
