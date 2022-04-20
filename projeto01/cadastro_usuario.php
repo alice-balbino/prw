0ffab548cad257f66d5dc21959fdb79f0b35b3a8
alice <aliceusaoculos@gmail.com>
2022-04-20 11:08:11 -0300

    cw

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -1,8 +1,9 @@
 <?php
+    include("conexao.php");
    $nome_usuario = $_POST['nome_usuario'];
    $email_usuario= $_POST['email_usuario'];
    $fone_usuario = $_POST['fone_usuario'];
-
+    echo "Cadastro de usuário:";
     echo "<p>Nome: ".$nome_usuario;
     echo "<p>email: ".$email_usuario;
     echo "<p>telefone: ".$fone_usuario;
