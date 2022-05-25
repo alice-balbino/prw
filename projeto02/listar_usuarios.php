3a1e4784e2c4ef4e4a7078ca6f99215d5ceab20d
alice <aliceusaoculos@gmail.com>
2022-05-25 11:38:31 -0300

    mudando

--- projeto02/listar_usuarios.php
+++ projeto02/listar_usuarios.php
@@ -23,7 +23,7 @@
 <body>
 
     <h1 align="center">Listagem de Usuários</h1><br>
-    <table align="center" border="1" width="1000">
+    <table align="center" border="1" width="1200">
         <!-- tr>th*4 -->
         <tr>
             <th>Código</th>
@@ -38,7 +38,7 @@
             while ($row = mysqli_fetch_array($result)) {
                 echo "<tr>";
                 echo "<td>" .$row['id_usuario']. "</td>";
-                echo "<td><img src='data:image/jpeg;base64,".base64_encode( $row["foto_blob"] )."' width='200' height='200'/></td>";
+                echo "<td><img src='data:image/jpeg;base64,".base64_encode( $row["foto_blob"] )."' width='150' height='150'/></td>";
                 echo "<td><a href='altera_usuario.php?id_usuario=".$row['id_usuario']."'>" .$row['nome_usuario']. "</a></td>";
                 echo "<td>" .$row['email_usuario']. "</td>";
                 echo "<td>" .$row['telefone_usuario']. "</td>";
